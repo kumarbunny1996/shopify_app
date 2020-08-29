ff16730d5bf9f3441bc81a625b2e8b69adedf6fa
iamsatish <kumarbunny3110@gmail.com>
2020-08-29 12:28:42 -0700

    db change

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('server is running at ' + port));
 
 //db connection
-const db = process.env.DB_URL_CONNECTION;
+const db = process.env.DB_URL_CONNECTION_PROD;
 mongoose.connect(db, {
         useNewUrlParser: true,
         useUnifiedTopology: true
